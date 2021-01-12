645e653f647e6b6f698ad8044d0493cd62afedb2
Doode1524 <Doode1524@gmail.com>
2021-01-12 16:22:33 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
     validates :content, length: {minimum: 250}
     validates :summary, length: {maximum: 250}
     validates :category, inclusion: {in: %w(Fiction)}
-    validates :non_clickbait?
+    validates :non_clickbait?, on: :create
 
     # def non_clickbait?
     #     if title: "True Facts"
